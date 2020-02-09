63cbac963192e48c6144509c8c8fbb8490c5bb7b
Jianzhou Feng <bravefjz@gmail.com>
2020-02-09 16:21:25 +0800

    modify test for plain text

--- dolphin_doc_lib/process_test.py
+++ dolphin_doc_lib/process_test.py
@@ -3,31 +3,30 @@ from typing import cast
 
 from dolphin_doc_lib.base.text import TextParagraph, TextSegment
 from dolphin_doc_lib.process import process, Content, ContentSource
-
-
-def _assert_block(block, text: str):
-    assert isinstance(block, TextParagraph)
-    assert len(block.segments()) == 1
-    assert block.segments()[0].to_dict() == TextSegment(text).to_dict()
+from dolphin_doc_lib.base.doc import Doc
 
 
 def test_plain_text():
     text = "paragraph 1\nparagraph 2\n\n  \n  \nparagraph 3\n"
     doc = process(Content(data=text))
-    blocks = doc.blocks()
-    assert len(blocks) == 3
-    _assert_block(blocks[0], "paragraph 1")
-    _assert_block(blocks[1], "paragraph 2")
-    _assert_block(blocks[2], "paragraph 3")
+
+    par1 = TextParagraph().append_text_segment(TextSegment("paragraph 1"))
+    par2 = TextParagraph().append_text_segment(TextSegment("paragraph 2"))
+    par3 = TextParagraph().append_text_segment(TextSegment("paragraph 3"))
+    expect_doc = Doc().append_blocks([par1, par2, par3])
+
+    assert doc == expect_doc
 
 
 def test_plain_text_from_file():
     doc = process(
         Content(source=ContentSource.FILE,
                 path="dolphin_doc_lib/testdata/plain_text.txt"))
-    blocks = doc.blocks()
-    assert len(blocks) == 4
-    _assert_block(blocks[0], "paragraph 1")
-    _assert_block(blocks[1], "paragraph 2")
-    _assert_block(blocks[2], "paragraph 3")
-    _assert_block(blocks[3], "paragraph 4")
+
+    par1 = TextParagraph().append_text_segment(TextSegment("paragraph 1"))
+    par2 = TextParagraph().append_text_segment(TextSegment("paragraph 2"))
+    par3 = TextParagraph().append_text_segment(TextSegment("paragraph 3"))
+    par4 = TextParagraph().append_text_segment(TextSegment("paragraph 4"))
+    expect_doc = Doc().append_blocks([par1, par2, par3, par4])
+
+    assert doc == expect_doc
